81ce17db3a672b8cdba0c300dfb0cc81ffb72cb0
Ben Pollock <ben.r.pollock@gmail.com>
2022-05-28 00:03:04 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { Component, useState } from 'react';
+import MyForm from './MyForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Information {
+  name: string;
+  age: string;
 }
+
+const App: FC = () => {
+  const [state, setState] = useState<Information | null>({
+    name: "ben",
+    age: "10",
+  });
+
+  return (
+        <div className="state-holder">
+          <MyForm state={state} />
+        </div>
+  );
+};
 
 export default App;
